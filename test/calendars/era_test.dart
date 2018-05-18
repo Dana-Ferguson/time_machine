287e4c2a4f8d3647d9d9e7e8df8d8ea4c378c11f
Joshua Ferguson <matterovermind@gmail.com>
2018-05-17 20:14:48 -0400

    Era unit tests -- kind of useless for us

---
+++ test/calendars/era_test.dart
@@ -0,0 +1,43 @@
+// https://github.com/nodatime/nodatime/blob/master/src/NodaTime/Calendars/Era.cs
+// 69dedbc  24 days ago
+
+import 'dart:async';
+import 'dart:math' as math;
+import 'dart:mirrors';
+
+import 'package:time_machine/time_machine.dart';
+import 'package:time_machine/time_machine_calendars.dart';
+import 'package:time_machine/time_machine_utilities.dart';
+
+import 'package:test/test.dart';
+import 'package:matcher/matcher.dart';
+import 'package:time_machine/time_machine_timezones.dart';
+
+import '../time_machine_testing.dart';
+
+Future main() async {
+  var eraType = reflectType(Era) as ClassMirror;
+  Eras = eraType
+      .declarations
+      .values
+      .where((v) => v is VariableMirror && v.isStatic)
+      .map((v) => eraType.getField(v.simpleName).reflectee)
+      .toList();
+
+  await runTests();
+}
+
+List<Era> Eras = []; /*typeof(Era).GetTypeInfo()
+    .DeclaredProperties // TODO: Only static and ones...
+    .Where(property => property.PropertyType == typeof(Era))
+.Select(property => property.GetValue(null, null))
+.Cast<Era>();*/
+
+@TestCaseSource(#Eras)
+@Test()
+void ResourcePresence(Era era)
+{
+  // todo: get us resources?
+  var valueByName = null; // PatternResources.ResourceManager.GetString(era.ResourceIdentifier, CultureInfo.InvariantCulture);
+  expect(valueByName, isNotNull, reason: "Missing resource for " + era?.resourceIdentifier ?? 'null');
+}
