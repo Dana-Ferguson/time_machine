2a3371e2ce4a710d7ff50618c9d7ed9d6befa018
Joshua Ferguson <matterovermind@gmail.com>
2018-07-05 02:22:58 -0400

    Split text functions back out into their own import.

--- lib/time_machine.dart
+++ lib/time_machine.dart
@@ -69,27 +69,6 @@ export 'src/text/globalization/datetime_format_info.dart' show CalendarType, Dat
 
 // Patterns (no public classes)
 
-// Text
-export 'src/text/composite_pattern_builder.dart' show CompositePatternBuilder;
-export 'src/text/i_pattern.dart' show IPattern;
-export 'src/text/parse_result.dart' show ParseResult;
-export 'src/text/unparsable_value_error.dart' show UnparsableValueError;
-export 'src/text/period_pattern.dart' show PeriodPattern;
-export 'src/text/invalid_pattern_error.dart' show InvalidPatternError;
-
-export 'src/text/localdate_pattern.dart' show LocalDatePattern;
-export 'src/text/localtime_pattern.dart' show LocalTimePattern;
-export 'src/text/localdatetime_pattern.dart' show LocalDateTimePattern;
-
-export 'src/text/annual_date_pattern.dart' show AnnualDatePattern;
-export 'src/text/duration_pattern.dart' show TimePattern;
-export 'src/text/instant_pattern.dart' show InstantPattern;
-export 'src/text/offset_date_pattern.dart' show OffsetDatePattern;
-export 'src/text/offset_datetime_pattern.dart' show OffsetDateTimePattern;
-export 'src/text/offset_pattern.dart' show OffsetPattern;
-export 'src/text/offset_time_pattern.dart' show OffsetTimePattern;
-export 'src/text/zoneddatetime_pattern.dart' show ZonedDateTimePattern;
-
 // TimeZones
 // todo: why is this public? (investigate)
 export 'src/timezones/datetimezone_cache.dart' show DateTimeZoneCache;
---
+++ lib/time_machine_text.dart
@@ -0,0 +1,27 @@
+// Portions of this work are Copyright 2018 The Time Machine Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0, as found in the LICENSE.txt file.
+
+// todo: investigate: https://github.com/pinkfish/flutter_native_timezone
+
+// https://github.com/dart-lang/sdk/issues/24581
+
+// Text and Parsing
+export 'src/text/composite_pattern_builder.dart' show CompositePatternBuilder;
+export 'src/text/i_pattern.dart' show IPattern;
+export 'src/text/parse_result.dart' show ParseResult;
+export 'src/text/unparsable_value_error.dart' show UnparsableValueError;
+export 'src/text/period_pattern.dart' show PeriodPattern;
+export 'src/text/invalid_pattern_error.dart' show InvalidPatternError;
+
+export 'src/text/localdate_pattern.dart' show LocalDatePattern;
+export 'src/text/localtime_pattern.dart' show LocalTimePattern;
+export 'src/text/localdatetime_pattern.dart' show LocalDateTimePattern;
+
+export 'src/text/annual_date_pattern.dart' show AnnualDatePattern;
+export 'src/text/duration_pattern.dart' show TimePattern;
+export 'src/text/instant_pattern.dart' show InstantPattern;
+export 'src/text/offset_date_pattern.dart' show OffsetDatePattern;
+export 'src/text/offset_datetime_pattern.dart' show OffsetDateTimePattern;
+export 'src/text/offset_pattern.dart' show OffsetPattern;
+export 'src/text/offset_time_pattern.dart' show OffsetTimePattern;
+export 'src/text/zoneddatetime_pattern.dart' show ZonedDateTimePattern;
