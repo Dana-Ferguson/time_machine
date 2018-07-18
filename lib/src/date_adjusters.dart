1e5f9a9d38117b7357b2a33a67a55339ca463569
Joshua Ferguson <matterovermind@gmail.com>
2018-07-17 23:09:09 -0400

    Date Adjusters Doc Formatting

--- lib/src/date_adjusters.dart
+++ lib/src/date_adjusters.dart
@@ -26,7 +26,8 @@ class DateAdjusters {
   /// The returned adjuster will throw an exception if it is applied to a date
   /// that would create an invalid result.
   ///
-  /// [day]: The day of month to adjust dates to.
+  /// * [day]: The day of month to adjust dates to.
+  ///
   /// An adjuster which changes the day to [day],
   /// retaining the same year and month.
   static LocalDate Function(LocalDate) dayOfMonth(int day) =>
@@ -38,7 +39,8 @@ class DateAdjusters {
   /// The returned adjuster will throw an exception if it is applied to a date
   /// that would create an invalid result.
   ///
-  /// [month]: The month to adjust dates to.
+  /// * [month]: The month to adjust dates to.
+  ///
   /// An adjuster which changes the month to [month],
   /// retaining the same year and day of month.
   static LocalDate Function(LocalDate) month(int month) =>
@@ -48,9 +50,10 @@ class DateAdjusters {
   /// A date adjuster to move to the next specified day-of-week, but return the
   /// original date if the day is already correct.
   ///
-  /// [dayOfWeek]: The day-of-week to adjust dates to.
+  /// * [dayOfWeek]: The day-of-week to adjust dates to.
+  ///
   /// An adjuster which advances a date to the next occurrence of the
-  /// specified day-of-week, or the original date if the day is already corret.
+  /// specified day-of-week, or the original date if the day is already correct.
   static LocalDate Function(LocalDate) nextOrSame(DayOfWeek dayOfWeek) {
     // Avoids boxing...
     if (dayOfWeek < DayOfWeek.monday || dayOfWeek > DayOfWeek.sunday) {
@@ -63,9 +66,10 @@ class DateAdjusters {
   /// A date adjuster to move to the previous specified day-of-week, but return the
   /// original date if the day is already correct.
   ///
-  /// [dayOfWeek]: The day-of-week to adjust dates to.
+  /// * [dayOfWeek]: The day-of-week to adjust dates to.
+  ///
   /// An adjuster which advances a date to the previous occurrence of the
-  /// specified day-of-week, or the original date if the day is already corret.
+  /// specified day-of-week, or the original date if the day is already correct.
   static LocalDate Function(LocalDate) previousOrSame(DayOfWeek dayOfWeek) {
     // Avoids boxing...
     if (dayOfWeek < DayOfWeek.monday || dayOfWeek > DayOfWeek.sunday) {
@@ -80,11 +84,11 @@ class DateAdjusters {
   ///
   /// This is the adjuster equivalent of [LocalDate.next].
   ///
-  /// [dayOfWeek]: The day-of-week to adjust dates to.
+  /// * [dayOfWeek]: The day-of-week to adjust dates to.
+  ///
   /// An adjuster which advances a date to the next occurrence of the
   /// specified day-of-week.
   static LocalDate Function(LocalDate) next(DayOfWeek dayOfWeek) {
-    // Avoids boxing...
     if (dayOfWeek < DayOfWeek.monday || dayOfWeek > DayOfWeek.sunday) {
       throw new RangeError.range(dayOfWeek.value, DayOfWeek.monday.value, DayOfWeek.sunday.value, 'dayOfWeek');
     }
@@ -97,11 +101,11 @@ class DateAdjusters {
   ///
   /// This is the adjuster equivalent of [LocalDate.previous].
   ///
-  /// [dayOfWeek]: The day-of-week to adjust dates to.
+  /// * [dayOfWeek]: The day-of-week to adjust dates to.
+  /// 
   /// An adjuster which advances a date to the previous occurrence of the
   /// specified day-of-week.
   static LocalDate Function(LocalDate) previous(DayOfWeek dayOfWeek) {
-    // Avoids boxing...
     if (dayOfWeek < DayOfWeek.monday || dayOfWeek > DayOfWeek.sunday) {
       throw new RangeError.range(dayOfWeek.value, DayOfWeek.monday.value, DayOfWeek.sunday.value, 'dayOfWeek');
     }
