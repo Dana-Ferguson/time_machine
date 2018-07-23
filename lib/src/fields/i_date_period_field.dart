ed06d19cedba51ac5503113f3bebc16d75529044
Joshua Ferguson <matterovermind@gmail.com>
2018-07-22 22:06:05 -0400

    DatePeriodField Doc Formatting

--- lib/src/fields/i_date_period_field.dart
+++ lib/src/fields/i_date_period_field.dart
@@ -12,18 +12,20 @@ abstract class IDatePeriodField {
   /// be reversible; for example, adding a month to January 30th will result in
   /// February 28th or February 29th.
   ///
-  /// [localDate]: The local date to add to
-  /// [value]: The value to add, in the units of the field
+  /// * [localDate]: The local date to add to
+  /// * [value]: The value to add, in the units of the field
+  ///
   /// Returns: The updated local date
   LocalDate add(LocalDate localDate, int value);
 
   /// Computes the difference between two local dates, as measured in the units
   /// of this field, rounding towards zero. This rounding means that
-  /// unit.Add(start, unit.UnitsBetween(start, end)) always ends up with a date
+  /// unit.Add(start, unit.unitsBetween(start, end)) always ends up with a date
   /// between start and end. (Ideally equal to end, but importantly, it never overshoots.)
   ///
-  /// [start]: The start date
-  /// [end]: The end date
+  /// * [start]: The start date
+  /// * [end]: The end date
+  ///
   /// Returns: The difference in the units of this field
   int unitsBetween(LocalDate start, LocalDate end);
 }
