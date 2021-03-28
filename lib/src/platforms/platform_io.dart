79d3e45bd8e753e70fa9a462e5820bf4a7cc20ef
Steve Alexander <steve@stevea.com>
2021-03-28 14:36:56 +0200

    restore a couple more @internal annotations

--- lib/src/platforms/platform_io.dart
+++ lib/src/platforms/platform_io.dart
@@ -11,11 +11,11 @@ import 'dart:typed_data';
 /// This class packages platform specific input-output functions that are initialized by the appropriate Platform Provider
 @internal
 abstract class PlatformIO {
-  /*@internal*/ Future<ByteData> getBinary(String path, String filename);
+  @internal Future<ByteData> getBinary(String path, String filename);
   // JSON.decode returns a dynamic -- will this change in Dart 2.0?
-  /* @internal */ Future<dynamic> getJson(String path, String filename);
+  @internal Future<dynamic> getJson(String path, String filename);
 
-  /*@internal*/ static late PlatformIO local;
+  @internal static late PlatformIO local;
 }
 
 Future initialize(dynamic arg) {
